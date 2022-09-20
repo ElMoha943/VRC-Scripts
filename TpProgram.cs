fbeb6d15a11df69e5245583b1068bebd6d8a7bcd
Valentino Casadidio <valentinodidio943@gmail.com>
2022-09-20 00:22:43 -0300

    Update TpProgram.cs

--- TpProgram.cs
+++ TpProgram.cs
@@ -1,4 +1,4 @@
-﻿using UdonSharp;
+using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -7,12 +7,15 @@ using UnityEngine.UI;
 public class TpProgram : UdonSharpBehaviour
 {
     public GameObject target;
+    public bool useWhitelist = false;
     public string[] StaffList; 
 
     public override void Interact(){
         VRCPlayerApi player = Networking.LocalPlayer;
         foreach (string name in StaffList)
-            if (player.displayName == name)
+            if (player.displayName == name || !useWhitelist){
                 Networking.LocalPlayer.TeleportTo(target.transform.position, target.transform.rotation);
+                break;
+            }
     }
 }
