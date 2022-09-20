95dc1036926f41e486a32c57c3f813d5fca3361b
Valentino Casadidio <valentinodidio943@gmail.com>
2022-09-20 00:19:25 -0300

    Add files via upload

---
+++ TpProgram.cs
@@ -0,0 +1,18 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+public class TpProgram : UdonSharpBehaviour
+{
+    public GameObject target;
+    public string[] StaffList; 
+
+    public override void Interact(){
+        VRCPlayerApi player = Networking.LocalPlayer;
+        foreach (string name in StaffList)
+            if (player.displayName == name)
+                Networking.LocalPlayer.TeleportTo(target.transform.position, target.transform.rotation);
+    }
+}
