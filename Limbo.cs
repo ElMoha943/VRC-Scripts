fb89bc742134d4c20e30a5bdddfc60c3a7c47283
Valen943 <valentinodidio943@gmail.com>
2023-01-03 21:11:51 -0300

    Add files via upload

---
+++ Limbo.cs
@@ -0,0 +1,50 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class Limbo : UdonSharpBehaviour
+{
+    [Tooltip("Object to move")]
+    public GameObject limboBar;
+    [Tooltip("Distance to move the object on Y axis")]
+    public float distance = 0.5f;
+    [Tooltip("Amount of levels the bar can go down")]
+    public int maxLevel = 10;
+
+    [UdonSynced(UdonSyncMode.None)]
+    int position; // 0 = original, up to 5 down
+
+    Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = limboBar.transform.position;
+    }
+
+    public void _moveDown()
+    {
+        Networking.SetOwner(Networking.LocalPlayer,gameObject);
+        if(position!=0){
+            position--;
+            RequestSerialization();
+            limboBar.transform.position = originalPosition + new Vector3(0, -distance * position, 0);
+        }
+    }
+
+    public void _moveUp()
+    {
+        Networking.SetOwner(Networking.LocalPlayer,gameObject);
+        if(position!=maxLevel){
+            position++;
+            RequestSerialization();
+            limboBar.transform.position = originalPosition + new Vector3(0, -distance * position, 0);
+        }
+    }
+
+    public override void OnDeserialization()
+    {
+        limboBar.transform.position = originalPosition + new Vector3(0, -distance * position, 0);
+    }
+}
