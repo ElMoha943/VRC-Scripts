2b4639a89c284773e08973e8e4271114581bbfd9
Valen943 <valentinodidio943@gmail.com>
2023-04-06 17:06:37 -0300

    Update SyncedToggle.cs

--- SyncedToggle.cs
+++ SyncedToggle.cs
@@ -9,22 +9,20 @@ public class SyncedToggle : UdonSharpBehaviour
     [SerializeField] GameObject[] objectsToToggle;
     [UdonSynced] bool state = false;
 
-
     void Start(){
         foreach (GameObject item in objectsToToggle)
             item.SetActive(state);
     }
 
-    public override void Interact()
-    {
+    public override void Interact(){
         Networking.SetOwner(Networking.LocalPlayer, gameObject);
         state = !state;
         foreach (GameObject item in objectsToToggle)
             item.SetActive(state);
+        RequestSerialization();
     }
 
-    public override void OnDeserialization()
-    {
+    public override void OnDeserialization(){
         foreach (GameObject item in objectsToToggle)
             item.SetActive(state);
     }
