c86c512b66ec3bb18d7464d63d2f33d58cba5d99
Valentino Casadidio <25410859+ElMoha943@users.noreply.github.com>
2023-07-04 21:25:46 -0300

    Update TpProgram.cs

--- TpProgram.cs
+++ TpProgram.cs
@@ -4,18 +4,20 @@ using VRC.SDKBase;
 using VRC.Udon;
 using UnityEngine.UI;
 
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class TpProgram : UdonSharpBehaviour
 {
-    public GameObject target;
-    public bool useWhitelist = false;
-    public string[] StaffList; 
+    [SerializeField] GameObject target;
+    [SerializeField] bool useWhitelist = false;
+    [SerializeField] string[] StaffList; 
 
     public override void Interact(){
         VRCPlayerApi player = Networking.LocalPlayer;
-        foreach (string name in StaffList)
+        foreach (string name in StaffList){
             if (player.displayName == name || !useWhitelist){
                 Networking.LocalPlayer.TeleportTo(target.transform.position, target.transform.rotation);
                 break;
             }
+        }
     }
 }
